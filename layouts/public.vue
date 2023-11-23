9e841fbd18d3e4afb67200f19779964aede9e82f
Miguel Bustillos <miguel.bustillos@onroadts.com>
2023-11-22 19:50:40 -0500

    feat: adding signup link in sign header

--- layouts/public.vue
+++ layouts/public.vue
@@ -40,7 +40,7 @@
                class="ml-6 inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
               Login
             </a>
-            <a href="/login"
+            <a href="/signup"
                class="ml-6 inline-flex items-center rounded-md border border-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
               Register
             </a>
